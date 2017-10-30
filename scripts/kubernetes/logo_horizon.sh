5e5d8860fbd86aec04eb7c810769cddf57ba33f8
lukepatrick <lukephilips@gmail.com>
2017-10-30 11:33:54 -0600

    pod by label

--- scripts/kubernetes/logo_horizon.sh
+++ scripts/kubernetes/logo_horizon.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 ## Push custom code into the Horizon container
-## TODO - add bootstrap abilities to Horizon Chard
+## TODO - add bootstrap abilities to Horizon Chart
 
 ## assumes this runs at a location that can access to kubectl
 
-export HORIZON=$(kubectl get pod -o name -n openstack | sed 's/pods\///' | grep horizon)
+export HORIZON=$(kubectl get pods --selector application=horizon -n openstack -o name | sed 's/pods\///')
 
 kubectl exec $HORIZON -n openstack -- curl -o /var/lib/kolla/venv/local/lib/python2.7/site-packages/horizon/templates/auth/_splash.html https://raw.githubusercontent.com/charter-ctec/horizon/stable/newton/horizon/templates/auth/_splash.html
 
