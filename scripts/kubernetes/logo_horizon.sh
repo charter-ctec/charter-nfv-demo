eabea4299727b1d0ebfc92408ff794eb9209e101
lukepatrick <lukephilips@gmail.com>
2017-10-30 11:45:37 -0600

    many horizon pods

--- scripts/kubernetes/logo_horizon.sh
+++ scripts/kubernetes/logo_horizon.sh
@@ -7,6 +7,9 @@
 
 export HORIZON=$(kubectl get pods --selector application=horizon -n openstack -o name | sed 's/pods\///')
 
-kubectl exec $HORIZON -n openstack -- curl -o /var/lib/kolla/venv/local/lib/python2.7/site-packages/horizon/templates/auth/_splash.html https://raw.githubusercontent.com/charter-ctec/horizon/stable/newton/horizon/templates/auth/_splash.html
-
-kubectl exec $HORIZON -n openstack -- curl -o /var/www/html/horizon/dashboard/img/charter_logo.jpeg https://raw.githubusercontent.com/charter-ctec/horizon/stable/newton/openstack_dashboard/static/dashboard/img/charter_logo.jpeg
+for item in ${HORIZON//\\n/ }
+do
+   echo "Item: $item"
+   kubectl exec $item -n openstack -- curl -o /var/lib/kolla/venv/local/lib/python2.7/site-packages/horizon/templates/auth/_splash.html https://raw.githubusercontent.com/charter-ctec/horizon/stable/newton/horizon/templates/auth/_splash.html
+   kubectl exec $item -n openstack -- curl -o /var/www/html/horizon/dashboard/img/charter_logo.jpeg https://raw.githubusercontent.com/charter-ctec/horizon/stable/newton/openstack_dashboard/static/dashboard/img/charter_logo.jpeg
+done
